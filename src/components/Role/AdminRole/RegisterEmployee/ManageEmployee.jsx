a0f964301f1bb285b6220bc46a191df70eca93c9
Dinh Truong <phamdinhtruong2511@gmail.com>
2023-04-17 10:52:26 +0700

    add manage employee

--- src/components/Role/AdminRole/RegisterEmployee/ManageEmployee.jsx
+++ src/components/Role/AdminRole/RegisterEmployee/ManageEmployee.jsx
@@ -1,7 +1,134 @@
-function ManageEmployee(){
-    return(
-        <div>
-            <h1>Manage Employee</h1>
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { calculateRange, sliceData } from "../../../../utils/table-pagination";
+import './styles.css';
+import GetJwtTokenClaim from "../../../../utils/JwtTokenClaim"
+
+function ManageEmployee() {
+    //#region Constants
+    const [error, setError] = useState("");
+    const [employeeList, setEmployeeList] = useState([]);
+    const payload = GetJwtTokenClaim();
+    const [search, setSearch] = useState('');
+    const [page, setPage] = useState(1);
+    const [pagination, setPagination] = useState([]);
+    const [originalEmployeeList, setOriginalEmployeeList] = useState([]);
+    //#endregion Constants
+    const handleGetListEmp = async () => {
+        try {
+            const url = "https://localhost:7199/api/employee";
+            const { data: res } = await axios.get(url, {
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                },
+            });
+            setEmployeeList(res);
+            setOriginalEmployeeList(res);
+            setPagination(calculateRange(res, 5));
+            setEmployeeList(sliceData(res, page, 5));
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
+    async function handleDelete(id) {
+        try {
+            await axios.delete(`https://localhost:7199/api/Employee/id?id=${id}`, {
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                },
+            });
+            setEmployeeList(employeeList.filter(request => request.Id !== id));
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
+    useEffect(() => {
+        handleGetListEmp();
+        setPagination(calculateRange(employeeList, 5));
+        setEmployeeList(sliceData(employeeList, page, 5));
+    }, [])
+
+    // Search
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+        if (event.target.value !== '') {
+            let search_results = originalEmployeeList.filter((item) =>
+                item.Id.toString().toLowerCase().includes(search.toLowerCase()) ||
+                item.FullName.toString().toLowerCase().includes(search.toLowerCase()) ||
+                item.Email.toString().toLowerCase().includes(search.toLowerCase())
+            );
+            setEmployeeList(search_results);
+            setPagination(calculateRange(search_results, 5));
+            setPage(1);
+        }
+        else {
+            setEmployeeList(sliceData(originalEmployeeList, page, 5));
+            setPagination(calculateRange(originalEmployeeList, 5));
+        }
+    };
+
+    // Change Page 
+    const handleChangePage = (new_page) => {
+        setPage(new_page);
+        setEmployeeList(sliceData(originalEmployeeList, new_page, 5));
+    }
+    return (
+        <div className="manage-employee-container">
+            <div className='manage-employee-header'>
+                <h1>Manage Employee</h1>
+                <input
+                    type='text'
+                    value={search}
+                    placeholder='Search..'
+                    className='register-employee-content-input'
+                    onChange={e => handleSearch(e)} />
+            </div>
+            {employeeList.length > 0 ?
+                <div className="employee-list">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                            {
+                                employeeList.map(request => (
+                                    <tr key={request.Id}>
+                                        <td>{request.Id}</td>
+                                        <td>{request.FullName}</td>
+                                        <td>{request.Email}</td>
+                                        <td>
+                                            <button className='btn-del' onClick={() => handleDelete(request.Id)} style={{ display: "block" }}>x</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div> : (
+                    <div style={{ padding: " 1em 0 0 1em" }}>No employee found.</div>
+                )
+            }
+            {employeeList.length !== 0 ?
+                <div className='dashboard-content-footer'>
+                    {pagination.map((item, index) => (
+                        <span
+                            key={index}
+                            className={item === page ? 'active-pagination' : 'pagination'}
+                            onClick={() => handleChangePage(item)}>
+                            {item}
+                        </span>
+                    ))}
+                </div>
+                :
+                <div className='dashboard-content-footer'>
+                    <span className='empty-table'>No data</span>
+                </div>
+            }
         </div>
     )
 }
--- src/components/Role/AdminRole/RegisterEmployee/RegisterEmployee.jsx
+++ src/components/Role/AdminRole/RegisterEmployee/RegisterEmployee.jsx
@@ -11,7 +11,7 @@ function RegisterEmployee() {
   const [page, setPage] = useState(1);
   const [pagination, setPagination] = useState([]);
   const [originalEmployeeRequests, setOriginalEmployeeRequests] = useState([]);
-  
+
   const handleGetListEmployeeRequests = async () => {
     try {
       const url = "https://localhost:7199/api/auth/get-list-employee-requests";
@@ -34,9 +34,9 @@ function RegisterEmployee() {
       setError(error.message);
     }
   }
-  async function handleAccept(id) {
+  function handleAccept(id) {
     try {
-      await axios.post(`https://localhost:7199/api/auth/register/employee?EmployeeRequestId=${id}`);
+      axios.post(`https://localhost:7199/api/auth/register/employee?EmployeeRequestId=${id}`);
       console.log(id);
       setEmployeeRequests(employeeRequests.filter(request => request.Id !== id));
     }
@@ -57,92 +57,88 @@ function RegisterEmployee() {
   const handleSearch = (event) => {
     setSearch(event.target.value);
     if (event.target.value !== '') {
-        let search_results = originalEmployeeRequests.filter((item) =>
-            item.Id.toString().toLowerCase().includes(search.toLowerCase()) ||
-            item.Name.toString().toLowerCase().includes(search.toLowerCase()) ||
-            item.Email.toString().toLowerCase().includes(search.toLowerCase())
-        );
-        setEmployeeRequests(search_results);
-        setPagination(calculateRange(search_results, 5));
-        setPage(1);
+      let search_results = originalEmployeeRequests.filter((item) =>
+        item.Id.toString().toLowerCase().includes(search.toLowerCase()) ||
+        item.Name.toString().toLowerCase().includes(search.toLowerCase()) ||
+        item.Email.toString().toLowerCase().includes(search.toLowerCase())
+      );
+      setEmployeeRequests(search_results);
+      setPagination(calculateRange(search_results, 5));
+      setPage(1);
     }
     else {
-        setEmployeeRequests(sliceData(originalEmployeeRequests, page, 5));
-        setPagination(calculateRange(originalEmployeeRequests, 5));
+      setEmployeeRequests(sliceData(originalEmployeeRequests, page, 5));
+      setPagination(calculateRange(originalEmployeeRequests, 5));
     }
   };
 
-      // Change Page 
-      const handleChangePage = (new_page) => {
-        setPage(new_page);
-        setEmployeeRequests(sliceData(originalEmployeeRequests, new_page, 5));
-      }
+  // Change Page 
+  const handleChangePage = (new_page) => {
+    setPage(new_page);
+    setEmployeeRequests(sliceData(originalEmployeeRequests, new_page, 5));
+  }
 
   return (
     <div className='register-request-container'>
       <div className="register-employee">
         <div className="table-request">
-          <div className="table-request-header" style={{paddingLeft:"1em"}}>
-            <h2>List of Employee Requests</h2>
+          <div className="table-request-header" style={{ paddingLeft: "1em", display: "flex" }}>
+            <h2>Employee Requests</h2>
+            <input
+              type='text'
+              value={search}
+              placeholder='Search..'
+              className='register-employee-content-input'
+              onChange={e => handleSearch(e)} />
           </div>
-          <div className='register-employee-content-header'>
-                    <div className='register-employee-content-search'>
-                        <input
-                            type='text'
-                            value={search}
-                            placeholder='Search..'
-                            className='register-employee-content-input'
-                            onChange={e => handleSearch(e)} />
-                    </div>
-                </div>
-          {employeeRequests.length > 0 ? 
-              <div className="employee-request-list">
-                <table>
-                  <tbody>
-                      <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                      <th>Email</th>
-                      <th>Action</th>
+          {employeeRequests.length > 0 ?
+            <div className="employee-request-list">
+              <table>
+                <tbody>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                  </tr>
+                  {
+                    employeeRequests.map(request => (
+                      <tr key={request.Id}>
+                        <td>{request.Id}</td>
+                        <td>{request.Name}</td>
+                        <td>{request.Email}</td>
+                        <td>
+                          <button className='btn-action' onClick={() => handleDelete(request.Id)} style={{ display: "block" }}>Reject</button>
+                          <button className='btn-action' onClick={() => handleAccept(request.Id)}>Accept</button>
+                        </td>
                       </tr>
-                      {
-                        employeeRequests.map(request => (
-                        <tr key={request.Id}>
-                            <td>{request.Id}</td>
-                            <td>{request.Name}</td>
-                            <td>{request.Email}</td>
-                            <td>
-                            <button className='btn-action' onClick={()=>handleDelete(request.Id)} style={{display:"block"}}>Reject</button>
-                            <button className='btn-action' onClick={()=>handleAccept(request.Id)}>Accept</button>
-                            </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>    
-              </div> :  (
-                          <div style={{padding:" 1em 0 0 1em"}}>No employee requests found.</div>
-                        )
+                    ))}
+                </tbody>
+              </table>
+            </div> : (
+              <div style={{ padding: " 1em 0 0 1em" }}>No employee requests found.</div>
+            )
           }
         </div>
         {employeeRequests.length !== 0 ?
-                    <div className='dashboard-content-footer'>
-                        {pagination.map((item, index) => (
-                            <span 
-                                key={index} 
-                                className={item === page ? 'active-pagination' : 'pagination'}
-                                onClick={() => handleChangePage(item)}>
-                                {item}
-                            </span>
-                        ))}
-                    </div>
-                : 
-                    <div className='dashboard-content-footer'>
-                        <span className='empty-table'>No data</span>
-                    </div>
-          }
-      </div>      
+          <div className='dashboard-content-footer'>
+            {pagination.map((item, index) => (
+              <span
+                key={index}
+                className={item === page ? 'active-pagination' : 'pagination'}
+                onClick={() => handleChangePage(item)}>
+                {item}
+              </span>
+            ))}
+          </div>
+          :
+          <div className='dashboard-content-footer'>
+            <span className='empty-table'>No data</span>
+          </div>
+        }
+      </div>
     </div>
   );
 }
-  
-  export default RegisterEmployee;
+
+export default RegisterEmployee;
